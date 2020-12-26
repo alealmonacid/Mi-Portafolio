452d3693d27b60b7bf21f44cfea0a61235fc0156
Alejandro <alejandroalmonaciderazo21@gmail.com>
2020-12-26 18:25:48 -0300

    Se añaden imagenes, se actualiza secciones tras error y se añade javascript con funciones de hover en logotipo

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Cambio de imagen Hover
+
+//mps
+
+$("#mps").mouseover(function()
+{this.src="assets/img/mpscolor.png"
+})
+.mouseout(function()
+{this.src="assets/img/mps.png"
+});
+
+//tibox
+
+$("#tibox").mouseover(function()
+{this.src="assets/img/tiboxcolor.png"
+})
+.mouseout(function()
+{this.src="assets/img/tibox.png"
+});
